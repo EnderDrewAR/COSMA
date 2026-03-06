4283f559d0bc3ae06ce0a682577095053a537d6a
Andrey801-afk <ivanovanbrey2017@gmail.com>
2026-03-06 21:48:58 +0300

    ref

--- Assets/_Project/Scripts/Satellite.cs
+++ Assets/_Project/Scripts/Satellite.cs
@@ -17,11 +17,26 @@ public class Satellite : MonoBehaviour
         _defaultRotation = transform.rotation;
     }
 
-    public void Rotate()
+    public void RotateRight()
     {
         _rigidbody.AddRelativeTorque(Vector3.up * 100 * Time.deltaTime * 10);
     }
+    
+    public void RotateLeft()
+    {
+        _rigidbody.AddRelativeTorque(Vector3.down * 100 * Time.deltaTime * 10);
+    }
+     
+    public void RotateUp()
+    {
+        _rigidbody.AddRelativeTorque(Vector3.forward * 100 * Time.deltaTime * 10);
+    }
 
+    public void RotateDown()
+    {
+        _rigidbody.AddRelativeTorque(-Vector3.forward * 100 * Time.deltaTime * 10);
+    }
+    
     public void Reset()
     {
         _rigidbody.linearVelocity = Vector3.zero;
